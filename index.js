9291766b22b37084f85a305e03a146f087c07612
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-11 16:45:50 -0700

    completed string lab 1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+var currentUser = "Alberto";
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const firstInitial = currentUser.charAt(0);
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser} !`;
+const shortGreeting = `Welcome, ${firstInitial}!`;
